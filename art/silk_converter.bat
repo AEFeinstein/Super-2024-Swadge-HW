913542f40bd1546054c452497dc29488813f44fb
AEFeinstein <gelakinetic@gmail.com>
2023-10-08 19:12:28 -0400

    Fixed silkscreen part numbers. Added clue. Bumped to v1.0.1

--- art/silk_converter.bat
+++ art/silk_converter.bat
@@ -1,1 +1,2 @@
 svg2mod.exe -i .\ship-silk.svg -o .\ship-silk.kicad_mod -c -d 3000 --format latest --factor 31.2495 -p 0.5
+svg2mod.exe -i .\silk_clue.svg -o .\silk_clue.kicad_mod -c -d 3000 --format latest --factor 31.2495 -p 0.5
